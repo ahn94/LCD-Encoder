4cbe29a33f764b32ceeece583e4913d10d2830d3
ahn94 <ahn94@hotmail.com>
2016-03-05 01:21:50 -0600

    Complete basic solution of 2 modes. Mode 0 - confetti has 4 options. Mode 1 solid has 3 options.

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -34,15 +34,17 @@ uint8_t nOptions[] = { 4, 3 };
 uint8_t currentOption;
 
 ClickEncoder *encoder;
-uint8_t last, value;
 uint8_t mode = 0;
-uint8_t CorB = 0;
 uint8_t clicked = 0;
 uint8_t dbclicked = 0;
 long timeOut;
 uint8_t interval = 80;
-uint16_t lastInteveral = 80;
-uint8_t color;
+uint8_t hue;
+uint8_t sat = 255;
+uint8_t bright = 100;
+uint8_t increment;
+
+
 
 
 
@@ -54,41 +56,32 @@ void timerIsr() {
 
 void setup()
 {
-	Serial.begin(115200);
+
+	// initialize LEDS
 	LEDS.setBrightness(100);
 	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	
 	// initialize library
 	lcd.begin(LCD_CHARS, LCD_LINES);
 	encoder = new ClickEncoder(A1, A0, A2, 4);
 	encoder->setAccelerationEnabled(true);
 	Timer1.initialize(1000);
 	Timer1.attachInterrupt(timerIsr);
-
-	// initialize timout
-	last = -1;
-	timeOut = millis();
-
-	// intitialize last delay to current so doesn't run till it changes
-	lastInteveral = interval;
-
+	
+	timeOut = millis(); // initilaze timout
+	
+	// initialize display
 	lcd.backlight();
-	// print starting screen
-	lcd.setCursor(0, 0);
-	// starts with value most used setting
-	lcd.print("color=");
-	lcd.setCursor(14, 0);
-	// print mode in top right
-	lcd.print('m');
-	lcd.print(mode);
+	display(0);
 	delay(1000);
 }
 
 
 void loop()
 {
+	increment = encoder->getValue();
 
-
-	displayMenu();
+	display(increment);
 
 	if (millis() > timeOut) {
 		timeOut = millis() + interval;
@@ -99,7 +92,7 @@ void loop()
 				confetti();
 				break;
 			case 1:
-				fill_solid(leds, NUM_LEDS, CHSV(value, 255, 255));
+				fill_solid(leds, NUM_LEDS, CHSV(hue, sat, bright));
 				break;
 		}
 	}
@@ -116,11 +109,10 @@ void loop()
 			break;
 		case ClickEncoder::DoubleClicked:
 			dbclicked++;
-			mode = dbclicked % NUM_MODES;
 			currentOption = 0;
-			lcd.setCursor(14, 0);
-			lcd.print('M');
-			lcd.print(mode);
+			clicked = 0;
+			mode = dbclicked % NUM_MODES;
+			display(0);
 			break;
 		case ClickEncoder::Held:
 			break;
@@ -129,23 +121,101 @@ void loop()
 
 }
 
+void display(uint8_t incr)
+{
+	switch (mode) // mode displayer switch
+	{
+		case 0: // confetti mode
+			switch (currentOption) // Options displayer switch
+			{
+				case 0: // color option
+					hue += incr;
+					adjustHue(); // print color display
+					break;
+				case 1: // saturation option
+					sat += incr; 
+					adjustSaturation(); // print saturation display
+					break;
+				case 2: // brightness option
+					bright += incr;
+					adjustBrightness(); // print brightness display 
+					break;
+				case 3: // interval option
+					interval += incr;
+					adjustInterval(); // print interval option
+					break;
+			}
+			lcd.setCursor(0, 1);
+			lcd.print("confetti");
+			break;
+		case 1: // solid color mode
+			switch (currentOption) // Options displayer switch
+			{
+				case 0: // color option
+					hue += incr;
+					adjustHue(); // print color display
+					break;
+				case 1: // saturation option
+					sat += incr;
+					adjustSaturation(); // print saturation display
+					break;
+				case 2: // brightness option
+					bright += incr;
+					adjustBrightness(); // print brightness display 
+					break;
+			}
+			lcd.setCursor(0, 1);
+			lcd.print("solid   ");
+			break;
+	}
+	// format/print second line of display
+	lcd.setCursor(10, 1);
+	lcd.print(currentOption + 1);
+	lcd.print(" of ");
+	lcd.print(nOptions[mode]);
+}
+
 void confetti()
 {
 	// random colored speckles that blink in and fade smoothly
 	fadeToBlackBy(leds, NUM_LEDS, 35);//long strip used five
 	int pos = random16(NUM_LEDS);
-	leds[pos] += CHSV(value + random8(64), 255, 255);
+	leds[pos] += CHSV(hue + random8(64), sat, bright);
 }
 
-void displayMenu()
-{
-	int increment;
-	value = encoder->getValue();
-	if (value != 0) {
-		last = value;
-		lcd.setCursor(6, 0);
-		lcd.print(value);
-		lcd.print("    ");
-	}
 
+void adjustHue()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("Hue=");
+	lcd.setCursor(4, 0);
+	lcd.print(hue);
+	lcd.print("         ");
+}
+
+void adjustSaturation()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("Saturation=");
+	lcd.setCursor(11, 0);
+	lcd.print(sat);
+	lcd.print("  ");
+}
+
+void adjustBrightness()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("Brightness=");
+	lcd.setCursor(11, 0);
+	lcd.print(bright);
+	lcd.print("  ");
+}
+
+void adjustInterval()
+{
+	lcd.setCursor(0, 0);
+	lcd.print("Interval=");
+	lcd.setCursor(9, 0);
+	lcd.print(interval);
+	lcd.print("    ");
 }
